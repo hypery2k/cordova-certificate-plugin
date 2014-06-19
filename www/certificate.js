1b7f02cd5a22a01c09df0f4146050e8224a417c6
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-06-19 06:13:44 +0200

    Fix js typo

--- www/certificate.js
+++ www/certificate.js
@@ -37,7 +37,7 @@ Certificate.prototype = {
      *      boolean value to set desired behaviour
      */
     trustUnsecureCerts: function (boolActivateUnsecure) {
-        cordova.exec(callback, null, 'CDVCertificate', 'setUntrusted', [boolActivateUnsecure]);
+        cordova.exec(null, null, 'CDVCertificate', 'setUntrusted', [boolActivateUnsecure]);
     }
 };
 
