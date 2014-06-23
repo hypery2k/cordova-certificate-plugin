b859242dde4c44f45beea3d30c42634839d6d7d9
Martin Reinhardt <contact@martinreinhardt-online.de>
2014-06-23 22:44:46 +0200

    Code cleanup
    new dev version

--- src/android/de/martinreinhardt/cordova/plugins/CertificatesCordovaWebViewClient.java
+++ src/android/de/martinreinhardt/cordova/plugins/CertificatesCordovaWebViewClient.java
@@ -1,43 +1,91 @@
+/*
+ 
+ The MIT License (MIT)
+ 
+ Copyright (c) 2014 Martin Reinhardt
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ Certificate Plugin for Cordova
+ 
+ */
 package de.martinreinhardt.cordova.plugins;
 
-
 import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaWebViewClient;
 
-import android.util.Log;
 import android.net.http.SslError;
+import android.util.Log;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 
-
 /**
- *
- * Certificate Plugin for Cordova
- *
- * author, Martin Reinhardt on 26.05.14.
- *
+ * 
+ * Certificates Cordova WebView Client
+ * 
+ * author, Martin Reinhardt on 23.06.14.
+ * 
+ * Copyright Martin Reinhardt 2014. All rights reserved.
+ * 
  */
 public class CertificatesCordovaWebViewClient extends CordovaWebViewClient {
 
+    /**
+     * Logging Tag
+     */
     public static final String TAG = "CertificatesCordovaWebViewClient";
 
     private boolean allowUntrusted = false;
 
+    /**
+     * 
+     * @param cordova
+     */
     public CertificatesCordovaWebViewClient(CordovaInterface cordova) {
         super(cordova);
     }
 
+    /**
+     * @return true of usage of untrusted (self-signed) certificates is allowed,
+     *         otherwise false
+     */
+    public boolean isAllowUntrusted() {
+        return allowUntrusted;
+    }
+
+    /**
+     * 
+     * 
+     * @param pAllowUntrusted
+     *            the allowUntrusted to set
+     */
     public void setAllowUntrusted(final boolean pAllowUntrusted) {
         this.allowUntrusted = pAllowUntrusted;
     }
 
-    public boolean isAllowUntrusted() {
-       return allowUntrusted;
-    }
-
+    /**
+     * @see org.apache.cordova.CordovaWebViewClient#onReceivedSslError(WebView,
+     *      SslErrorHandler, SslError)
+     */
     @Override
-    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+    public void onReceivedSslError(WebView view, SslErrorHandler handler,
+            SslError error) {
         Log.d(TAG, "onReceivedSslError. Proceed? " + isAllowUntrusted());
         if (isAllowUntrusted()) {
             handler.proceed();
--- src/android/de/martinreinhardt/cordova/plugins/CertificatesPlugin.java
+++ src/android/de/martinreinhardt/cordova/plugins/CertificatesPlugin.java
@@ -1,45 +1,84 @@
+/*
+ 
+ The MIT License (MIT)
+ 
+ Copyright (c) 2014 Martin Reinhardt
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ 
+ Certificate Plugin for Cordova
+ 
+ */
 package de.martinreinhardt.cordova.plugins;
 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaActivity;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import android.util.Log;
-import org.apache.cordova.*;
-import org.json.*;
-import android.content.Context;
 
 /**
- *
+ * 
  * Certificate Plugin for Cordova
  * 
- * author, Martin Reinhardt on 26.05.14.
+ * author, Martin Reinhardt on 23.06.14.
+ * 
+ * Copyright Martin Reinhardt 2014. All rights reserved.
  * 
  */
 public class CertificatesPlugin extends CordovaPlugin {
 
+    /**
+     * Logging Tag
+     */
     private static final String LOG_TAG = "Certificates";
 
     /**
      * Executes the request.
-     *
-     * This method is called from the WebView thread. To do a non-trivial amount of work, use:
-     *     cordova.getThreadPool().execute(runnable);
-     *
+     * 
+     * This method is called from the WebView thread. To do a non-trivial amount
+     * of work, use: cordova.getThreadPool().execute(runnable);
+     * 
      * To run on the UI thread, use:
-     *     cordova.getActivity().runOnUiThread(runnable);
-     *
-     * @param action          The action to execute. (Currently "setUntrusted only")
-     * @param args            The exec() arguments.
-     * @param callbackContext The callback context used when calling back into JavaScript.
-     * @return                Whether the action was valid.
-     *
-     *
+     * cordova.getActivity().runOnUiThread(runnable);
+     * 
+     * @param action
+     *            The action to execute. (Currently "setUntrusted only")
+     * @param args
+     *            The exec() arguments.
+     * @param callbackContext
+     *            The callback context used when calling back into JavaScript.
+     * @return Whether the action was valid.
+     * 
+     * 
      */
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, JSONArray args,
+            CallbackContext callbackContext) throws JSONException {
         if (action.equals("setUntrusted")) {
             boolean allowUntrusted = args.getBoolean(0);
             Log.d(LOG_TAG, "Setting allowUntrusted to " + allowUntrusted);
             CordovaActivity ca = (CordovaActivity) this.cordova.getActivity();
-            CertificatesCordovaWebViewClient cWebClient = new CertificatesCordovaWebViewClient(this.cordova);
+            CertificatesCordovaWebViewClient cWebClient = new CertificatesCordovaWebViewClient(
+                    this.cordova);
             cWebClient.setAllowUntrusted(allowUntrusted);
             webView.setWebViewClient(cWebClient);
             ca.clearCache();
@@ -48,8 +87,5 @@ public class CertificatesPlugin extends CordovaPlugin {
         }
         callbackContext.error("Invalid Command");
         return false;
-    }
-    
-    public void setUntrusted() {
     }
 }
