6b2fdd0c986adf5b59d8b7d79b8e9e7943a520e7
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-04-28 08:58:04 +0200

    Moving tests to shellscript

---
+++ runIntegrationTests.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+cwd=$(pwd)
+cd ..  
+rm -rf cordova-test-app
+git clone https://github.com/hypery2k/cordova-demo-app.git cordova-test-app
+cd cordova-test-app
+npm install
+bower install
+grunt "ci:$PLATFORM" "$PLATFORM"
+cordova plugin add ../cordova-certificate-plugin/
+echo $cwd
