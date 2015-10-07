2edc6999831ffe561d2d9ef5e91fad3c20088180
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-10-07 10:42:11 +0200

    Updating tests for cordova v4 see #6 and #8

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -6,7 +6,11 @@ git clone https://github.com/hypery2k/cordova-demo-app.git cordova-test-app
 cd cordova-test-app
 npm install
 bower install
+echo "Building vanilla cordova app"
 grunt "ci:$PLATFORM" "$PLATFORM"
+echo "Adding plugin"
 cordova plugin add ../cordova-certificate-plugin/
+echo "Building vanilla cordova app with added plugin"
+grunt "$PLATFORM"
 echo "Changing back to plugin directy: "$cwd
 cd $cwd
